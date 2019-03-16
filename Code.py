8350245b3e494fe27b552f58e4ac6bec958cd1f0
hdhawan1 <hdhawan@uwo.ca>
2019-03-16 18:50:52 -0400

    update with multi windows

--- Code.py
+++ Code.py
@@ -1,10 +1,50 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
-root = Tk()
-frame = Frame(root)
+def try_login():               # this my login function
+    if username.get()==default_name and password.get() == default_password:
+       log.destroy()
+
+    else:
+       messagebox.showwarning("Login failed","Please try again")
+
+def cancel_login():
+    log.destroy()
+
+default_name=("Ryan Li")      #DEFAULT LOGIN ENTRY
+default_password=("rli123")
+
+log=Tk()                   #this login ui
+log.title("basq't")
+log.geometry("260x400")
+log.resizable (width=FALSE,height=FALSE)
+
+frame = Frame(log)
 frame.pack()
 
-bottomframe = Frame(root)
+upperframe = LabelFrame(log, height = 125)
+upperframe.pack(side = TOP)
+midframe = Frame(log)
+midframe.pack(side = TOP)
+lowframe = LabelFrame(log, height = 180)
+lowframe.pack(side = TOP)
+uname = StringVar()
+username = Entry(midframe, textvariable = uname)
+username.pack()
+pword = StringVar()
+password = Entry(midframe, show = "*", textvariable = pword)
+password.pack()
+cont = Button(midframe, text = "Continue -->", command = lambda: try_login())
+cont.pack()
+
+log.mainloop()
+
+mainwindow = Tk()
+mainwindow.title("basq't")
+mainwindow.geometry("260x400")
+mainwindow.resizable (width=FALSE,height=FALSE)
+bottomframe = Frame(mainwindow)
 bottomframe.pack(side = BOTTOM)
 
 redbutton = Button(bottomframe, text="Red", bg = "black", fg="white", relief = GROOVE, height = 3, width = 6)
@@ -22,5 +62,5 @@ bluebutton.pack(side = LEFT)
 blackbutton = Button(bottomframe, text="Black", bg = "black", fg="white",relief=GROOVE, height = 3, width = 6)
 blackbutton.pack(side = LEFT)
 
-root.maxsize(260,600)
-root.mainloop()
+
+mainwindow.mainloop()
